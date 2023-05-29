a8e5ccd2c51bcbacea713a72114ccace7b489403
kumankoishi.py <7iggif@naver.com>
2023-05-30 05:49:07 +0900

    init(main): set global style and main style

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Root = styled.div`
+  box-sizing: border-box;
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 function Main() {
   return (
-    <>Hello!</>
+    <Root>Hello!</Root>
   );
 }
 
