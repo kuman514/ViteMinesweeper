c2900fb61b483bc1a3bde3be2547468bb99d37b3
kumankoishi.py <7iggif@naver.com>
2023-06-04 00:06:04 +0900

    draft(game): define types

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export interface GameConfig {
+  width: number;
+  height: number;
+  mines: number;
+}
+
+export interface GameStoreState {
+  width: number;
+  height: number;
+  mines: number;
+  remainingMines: number;
+  isMine: boolean[][];
+  mineAroundCount: number[][];
+  isVisited: boolean[][];
+  isMarkedAsMine: boolean[][];
+  isInit: boolean;
+  isContinuable: boolean;
+}
+
+export interface GameStoreAction {
+  resetGame(config: GameConfig): void;
+  initClick(row: number, col: number): void;
+  click(row: number, col: number): void;
+}
+
+export type GameStore = GameStoreState & GameStoreAction;
