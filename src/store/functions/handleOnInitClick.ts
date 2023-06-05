a539ee1227009f2d4e85f1424f335ca55d9e836e
kumankoishi.py <7iggif@naver.com>
2023-06-05 09:57:05 +0900

    draft(game): add handle on-init-click

---
+++ src/store/functions/handleOnInitClick.ts
@@ -0,0 +1,70 @@
+import { direction8 } from '^/constants/direction';
+import { GameStoreState } from '^/types';
+import { generateInitStateArray } from '^/utils/array';
+import { generateRandomInteger } from '^/utils/random';
+
+interface Parameter {
+  gameStoreState: GameStoreState;
+  row: number;
+  col: number;
+}
+
+export function handleOnInitClick({ gameStoreState, row, col }: Parameter): GameStoreState {
+  const {
+    isInit,
+    isContinuable,
+    width,
+    height,
+    mines,
+  } = gameStoreState;
+
+  if (!isInit || !isContinuable) {
+    return gameStoreState;
+  }
+
+  const newIsMine = generateInitStateArray<boolean>(width, height, false);
+  const maxRange = width * height - 1;
+  const except = width * row + col;
+  Array.from(
+    { length: mines },
+    () => generateRandomInteger(0, maxRange, except),
+  ).forEach((mineIndex) => {
+    const mineRow = Math.floor(mineIndex / width);
+    const mineCol = mineIndex % width;
+    newIsMine[mineRow][mineCol] = true;
+  });
+
+  const newMineAroundCount = generateInitStateArray<number>(width, height, 0);
+  newIsMine.forEach((rowLine, i) => {
+    rowLine.forEach((isThisMine, j) => {
+      if (isThisMine) {
+        newMineAroundCount[i][j] = 9;
+        return;
+      }
+      direction8.forEach(({ row: rowDir, col: colDir }) => {
+        const searchRow = i + rowDir;
+        const searchCol = j + colDir;
+
+        if (searchRow < 0 || searchRow >= height || searchCol < 0 || searchCol >= width) {
+          return;
+        }
+
+        if (newIsMine[searchRow][searchCol]) {
+          newMineAroundCount[i][j]++;
+        }
+      });
+    });
+  });
+
+  /**
+   * @todo
+   * Implement handleOnClick first
+   */
+
+  return {
+    ...gameStoreState,
+    isMine: newIsMine,
+    mineAroundCount: newMineAroundCount,
+    isInit: false,
+  };
+}
