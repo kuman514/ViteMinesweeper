bec57c97a8ffc6ed0d6a3cf14c4f834abb1bdebf
kumankoishi.py <7iggif@naver.com>
2023-06-05 09:56:23 +0900

    draft(game): add direction constant

---
+++ src/constants/direction.ts
@@ -0,0 +1,10 @@
+export const direction8 = [
+  { row: 1, col: 0 },
+  { row: 1, col: 1 },
+  { row: 0, col: 1 },
+  { row: -1, col: 1 },
+  { row: -1, col: 0 },
+  { row: -1, col: -1 },
+  { row: 0, col: -1 },
+  { row: 1, col: -1 },
+];
